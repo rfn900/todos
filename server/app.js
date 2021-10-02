0b00db1aca9d17fb09ac5392acae7a73269e1f70
rfn900 <rfn900@gmail.com>
2021-10-02 20:42:53 +0200

    Add dependencies on the server and clean boilerplate

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const app = express();
 
 var app = express();
 
