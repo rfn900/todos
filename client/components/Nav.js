71a0dc43c43c852379d37fc30e9855213fbd917f
rfn900 <rfn900@gmail.com>
2021-10-07 13:04:54 +0200

    Add google profile foto in Navbar

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,13 +1,17 @@
 import { DocumentTextIcon } from "@heroicons/react/outline";
-export const Nav = () => {
+export const Nav = ({ user }) => {
   return (
     <nav className="flex px-8 items-center justify-between border-b-2 py-2">
       <div className="flex items-center justify-around gap-4">
         <DocumentTextIcon className="h-10 py-2 px-1 bg-yellow-300 text-white" />
         <h3 className="text-2xl font-light text-gray-600">Todos</h3>
       </div>
-      <div className="h-10 w-10 rounded-full shadow ">
-        <img src="/profile.png" alt="Profile bild" className="object-contain" />
+      <div className="h-10 w-10 overflow-hidden rounded-full shadow ">
+        <img
+          src={user.imageUrl}
+          alt="Profile bild"
+          className="object-contain"
+        />
       </div>
     </nav>
   );
